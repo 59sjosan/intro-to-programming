5d67e95bbc740fdd20cf9fb4eb323cc89bc64164
59sjosan <josansejal2002@gmail.com>
2023-03-31 10:49:09 -0400

    v2

---
+++ problem2.py
@@ -0,0 +1,17 @@
+def takeinput():
+    a=int(input("1st Number: "))
+    b=int(input("2nd Number: "))
+    opr=input("Operator: ")
+    if opr=='+':
+        displayResult(a+b)
+    elif opr=='*':
+        displayResult(a*b)
+    elif opr=='-':
+        displayResult(a-b)
+    elif opr=='/':
+        displayResult(a/b)
+def displayResult(c):
+    print("Output is ",c)
+
+takeinput()
+
